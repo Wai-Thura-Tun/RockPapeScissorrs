6d6d5b7d1b46317126589752dfaa2e1574411102
Wai-Thura-Tun <maxw52493@gmail.com>
2023-11-04 00:31:00 +0630

    Initial Commit

---
+++ rockpaperscissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rockpaperscissors
+//
+//  Created by Wai Thura Tun on 04/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rockpaperscissors/rockpaperscissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockpaperscissorsApp.swift
+//  rockpaperscissors
+//
+//  Created by Wai Thura Tun on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct rockpaperscissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
